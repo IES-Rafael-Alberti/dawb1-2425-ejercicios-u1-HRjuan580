f0d70f1f1bc558ce8d77783ff4a903ace0a58d90
HRjuan580 <hrjuan@iesrafaelalberti.es>
2024-09-27 21:34:16 +0200

    Ejercicio 3 hecho

---
+++ src/ej03.py
@@ -0,0 +1,16 @@
+# Sentencia de asignación
+ancho = 17
+alto = 12.0 
+
+# Valor de las expresiones
+# 1. ancho / 2 
+# 2. ancho // 2
+# 3. alto / 3
+# 4. 1 + 2 * 5
+
+# Comprobar el interprete si has hacertado
+
+print(type(ancho/2))
+print(type(ancho // 2))
+print(type(alto / 3))
+print(type(1 + 2 * 5))

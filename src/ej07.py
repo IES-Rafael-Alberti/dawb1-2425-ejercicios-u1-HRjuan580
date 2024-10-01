521ac5025a6a7017363b4a7f6b20531d7a5acbd8
HRjuan580 <hrjuan@iesrafaelalberti.es>
2024-10-01 16:39:45 +0200

    ejercico 7,8 hechos

---
+++ src/ej07.py
@@ -0,0 +1,12 @@
+# Solicita 3 numeros
+numero1 = float(input("Introduce un número:\n"))
+numero2 = float(input("Introduce un número:\n"))
+numero3 = float(input("Introduce un número:\n"))
+
+# Suma los numeros ss
+
+suma = numero1 + numero2 + numero3
+
+# La suma de los numeros es
+
+print("La suma de los numeros es:",suma)
---
+++ src/ej09.py
@@ -0,0 +1,11 @@
+# Solicita 3 numeros
+numero1 = float(input("Introduce un número:\n"))
+numero2 = float(input("Introduce un número:\n"))
+numero3 = float(input("Introduce un número:\n"))
+
+# Suma los numeros ss
+
+
+# La suma de los numeros es
+
+print("La suma de los numeros es:",suma)

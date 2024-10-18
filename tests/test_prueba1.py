f7426c55d82e63542c6bf0d3dbc0351ed5f584f1
HRJuan580 <jherrel580@g.educaand.es>
2024-10-18 13:31:39 +0200

    nuevo

--- tests/test_prueba1.py
+++ tests/test_prueba1.py
@@ -1,1 +1,11 @@
-    
+import unittest
+from src.prueba1 import suma
+
+class TestPrueba1(unittest.TestCase):
+    def test_suma(self):
+        self.assertEqual(suma(1, 2), 3)
+        self.assertEqual(suma(-1, 1), 0)
+        self.assertEqual(suma(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
